c12da04d247756a389b178fc8018ab2579e72fd6
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2022-06-03 15:02:21 +0100

    remove HTTPClient access from graphql.client

--- graphql/client/__init__.py
+++ graphql/client/__init__.py
@@ -2,12 +2,9 @@ from graphql.client.client import *
 from graphql.client.client import __all__ as _client__all__
 from graphql.client.errors import *
 from graphql.client.errors import __all__ as _errors__all__
-from graphql.client.http import *
-from graphql.client.http import __all__ as _http__all__
 
 
 __all__ = [  # pyright: ignore[reportUnsupportedDunderAll]
     *_client__all__,
     *_errors__all__,
-    *_http__all__,
 ]
