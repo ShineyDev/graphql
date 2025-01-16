cafd9d2918d41f7a7d00b6f30d27a025800cbc1d
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-16 16:57:48 +0000

    export client.http

--- graphql/client/__init__.py
+++ graphql/client/__init__.py
@@ -2,9 +2,11 @@ from graphql.client.client import *
 from graphql.client.client import __all__ as _client__all__
 from graphql.client.errors import *
 from graphql.client.errors import __all__ as _errors__all__
+from graphql.client import http as http
 
 
 __all__ = [  # pyright: ignore[reportUnsupportedDunderAll]
     *_client__all__,
     *_errors__all__,
+    "http",
 ]
